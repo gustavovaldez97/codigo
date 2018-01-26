1eb51b526b3d29438e18d2cebe6dd1e739a83b2b
Gustavo Lazcano Valdez <quitin9712@hotmail.com>
2018-01-25 21:52:04 -0600

    Commit Inicial

---
+++ Gustavo/glv4.c
@@ -0,0 +1,11 @@
+//GLV
+//Tarea 4
+#include <stdio.h>
+main ()
+{
+    char letter;
+    printf ("Ingresa un caracter");
+    scanf("\n%c", &letter);
+    printf ("El caracter %c corresponde al numero %d en ASCII", letter, letter);
+    return 0;
+    }
